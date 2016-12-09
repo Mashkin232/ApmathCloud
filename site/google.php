ee9364c2548f4be500b8a7df40765192a655757a
Mashkin232 <0913757@mail.ru>
2016-12-09 21:19:57 +0300

    Update google.php

--- site/google.php
+++ site/google.php
@@ -1,34 +1,68 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Поиск по сайту с использованием Google | Демонстрация для сайта RUSELLER.COM</title>
-<link rel="stylesheet" type="text/css" href="styles.css" />
-</head>
-<body>
-<div id="page">
-<h1>Поиск по сайту с использованием Google</h1>
-<form id="searchForm" method="post">
-<fieldset>
-<input id="s" type="text" />
-<input type="submit" value="Submit" id="submitButton" />
-<div id="searchInContainer">
-<input type="radio" name="check" value="site" id="searchSite" checked />
-<label for="searchSite" id="siteNameLabel">Поиск по</label>
-<input type="radio" name="check" value="web" id="searchWeb" />
-<label for="searchWeb">Поиск в Интернет</label>
-</div>
-<ul class="icons">
-<li class="web" title="Страницы" data-searchType="web">Страницы</li>
-<li class="images" title="Изображения" data-searchType="images">Изображения</li>
-<li class="news" title="Новости" data-searchType="news">Новости</li>
-<li class="videos" title="Видео" data-searchType="video">Видео</li>
-</ul>
-</fieldset>
-</form>
-<div id="resultsDiv"></div>
-</div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script src="script.js"></script>
-</body>
-</html>
+<?php
+/* Accessing the Google Web API via PHP
+   - by Simon Willison (simon@incutio.com)
+   This code is in the public domain - do whatever you want with it
+
+   To use this code you will need both PEAR and the PEAR SOAP package
+   installed somewhere on your php include path. You can get the SOAP
+   package from CVS:
+
+   cvs -d :pserver:cvsread@cvs.php.net:/repository login
+   (enter phpfi as the password)
+   cvs -d :pserver:cvsread@cvs.php.net:/repository co pear/SOAP
+
+   You will need a Google license key - see this site:
+      http://www.google.com/apis/
+
+*/
+
+include("SOAP/Client.php");
+
+// Google search query
+$query = 'soap';
+
+// Your google license key
+$key = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
+
+$s = new SOAP_Client('http://api.google.com/search/beta2');
+$result = $s->call('doGoogleSearch', array(
+    'key' => $key,
+    'q' => $q,
+    'start' => 0,
+    'maxResults' => 10,
+    'filter' => false,
+    'restrict' => '',
+    'safeSearch' => false,
+    'lr' => '',
+    'ie' => '',
+    'oe' => '',
+), 'urn:GoogleSearch');
+
+// Is result a PEAR_Error?
+if (get_class($result) == 'pear_error')
+{
+	$message = $result->message;
+	$output = "An error occured: $message<p>";
+}
+else
+{
+	// We have proper search results
+	$num = $result['estimatedTotalResultsCount'];
+	$elements = $result['resultElements'];
+	$list = '';
+	if ($num > 0) {
+		foreach ($elements as $item) {
+			$size = $item['cachedSize'];
+			$title = $item['title'];
+			$url = $item['URL'];
+			$snippet = $item['snippet'];
+			$desc = "<p><b>$title</b> - <a href=\"$url\">$url</a> ";
+			$desc .= "<small>[Size: $size]</small></p>";
+			$desc .= "\n<blockquote>$snippet</blockquote>\n\n";
+			$list .= $desc;
+		}
+	}
+	$output = "$num results returned:\n\n$list";
+}
+echo $output;
+?>
