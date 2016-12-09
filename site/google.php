ca185951eee198934eef3c6d03e6a2133241f9c1
Mashkin232 <0913757@mail.ru>
2016-12-09 21:25:19 +0300

    Update google.php

--- site/google.php
+++ site/google.php
@@ -1,74 +1,82 @@
-<html>
-	<head>
-		<title>php</title>
-	</head>
-	<body><?php
-/* Accessing the Google Web API via PHP
-   - by Simon Willison (simon@incutio.com)
-   This code is in the public domain - do whatever you want with it
+<?php
+//Получаем настройки поискового запроса
+$data    = MyDB::get() -> selectOne('*',self::TABLE,'`id_mod` = '.$this->id_mod);
 
-   To use this code you will need both PEAR and the PEAR SOAP package
-   installed somewhere on your php include path. You can get the SOAP
-   package from CVS:
+//На каком сайте ищем?
+$sireUrl = $data['url'];
 
-   cvs -d :pserver:cvsread@cvs.php.net:/repository login
-   (enter phpfi as the password)
-   cvs -d :pserver:cvsread@cvs.php.net:/repository co pear/SOAP
+//Данные полученные от пользователя
+$sigs = array(
+  'q'    => array('type' => 'string', 'required' => false),
+  'start'  => array('type' => 'integer', 'required' => false)
+);
+$reqData = SpeData::sanitize_vars($this->queryArray, $sigs, 'RequestException');    
+$q     = urlencode($reqData['q'].' site:'.$sireUrl);
+$start   = empty($reqData['start']) ? 0 : $reqData['start']; 
 
-   You will need a Google license key - see this site:
-      http://www.google.com/apis/
+//Отправляем запрос гуглу, собственно это основаня часть :)
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$q&rsz=large&hl=ru&start=$start");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_REFERER, "http://$sireUrl/");
+$body = curl_exec($ch);
+curl_close($ch);
 
-*/
+//Ответ получили, в принципе, зада выполнена :)
+$json = json_decode($body);
+//На этом этапе можно сделать print_r($json) и все станет понятно
+//Но что бы в smarty было проще разобрать результаты, можно преобразовать его в следующий вид
 
-include("SOAP/Client.php");
+$search = array();
 
-// Google search query
-$query = 'soap';
-
-// Your google license key
-$key = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
-
-$s = new SOAP_Client('http://api.google.com/search/beta2');
-$result = $s->call('doGoogleSearch', array(
-    'key' => $key,
-    'q' => $q,
-    'start' => 0,
-    'maxResults' => 10,
-    'filter' => false,
-    'restrict' => '',
-    'safeSearch' => false,
-    'lr' => '',
-    'ie' => '',
-    'oe' => '',
-), 'urn:GoogleSearch');
-
-// Is result a PEAR_Error?
-if (get_class($result) == 'pear_error')
-{
-	$message = $result->message;
-	$output = "An error occured: $message<p>";
+//Результаты поиска
+if (count($json -> responseData -> results) == 0) { //если ничего не найдено
+  $search['result'] = false;
+} else {
+  foreach ($json -> responseData -> results as $v) {
+    $search['result'][] = array(
+      'GsearchResultClass'   => $v -> GsearchResultClass,
+      'unescapedUrl'       => $v -> unescapedUrl,
+      'url'           => $v -> url,
+      'visibleUrl'       => $v -> visibleUrl,
+      'cacheUrl'         => $v -> cacheUrl,
+      'title'         => $v -> title, //заголовок найденого документа (индексируется ведь не только html-странички)
+      'titleNoFormatting'   => $v -> titleNoFormatting,
+      'content'         => $v -> content //выдержка из текста документа
+    ); 
+  }
 }
-else
-{
-	// We have proper search results
-	$num = $result['estimatedTotalResultsCount'];
-	$elements = $result['resultElements'];
-	$list = '';
-	if ($num > 0) {
-		foreach ($elements as $item) {
-			$size = $item['cachedSize'];
-			$title = $item['title'];
-			$url = $item['URL'];
-			$snippet = $item['snippet'];
-			$desc = "<p><b>$title</b> - <a href=\"$url\">$url</a> ";
-			$desc .= "<small>[Size: $size]</small></p>";
-			$desc .= "\n<blockquote>$snippet</blockquote>\n\n";
-			$list .= $desc;
-		}
-	}
-	$output = "$num results returned:\n\n$list";
+
+//Список ссылок на остальные результаты поиска 
+if (count($json -> responseData -> results) == 0) { //если ничего не найдено
+  $search['pages'] = false;
+} else {    
+  $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REDIRECT_URL'].'?q='.$reqData['q'];
+  foreach ($json -> responseData -> cursor -> pages as $v) {
+    $search['pages'][] = array(
+      'start'   => $v -> start,
+      'startUrl'  => $url.'&start='.$v -> start,
+      'label'   => $v -> label
+    );
+  }
 }
-echo $output;
+
+//Общая информация о результатах поиска
+$currentPageIndex = $json -> responseData -> cursor -> currentPageIndex;
+$search['info'] = array(
+  'q'            => $reqData['q'],
+  'estimatedResultCount'   => $json -> responseData -> cursor -> estimatedResultCount,
+  'moreResultsUrl'    => $json -> responseData -> cursor -> moreResultsUrl,
+  'currentPageIndex'     => $currentPageIndex,
+  'currentLabel'      => $currentPageIndex + 1,
+  'startResult'      => $currentPageIndex * 8 + 1,
+  'endResult'        => ($currentPageIndex * 8 + 1) + count($search['result']),
+  'next'          => (count($search['pages']) > $currentPageIndex + 1) ? $search['pages'][$currentPageIndex + 1]['startUrl'] : false,
+  'prev'          => ($currentPageIndex) ? $search['pages'][$currentPageIndex - 1]['startUrl'] : false
+);
+  
+
+//Все готово
+MySmarty::get() -> assign('search', $search);
+
 ?>
-	</body>
-</html>
