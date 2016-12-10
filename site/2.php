504fc906337940817fd094951883a1463bbb9f91
Mashkin232 <0913757@mail.ru>
2016-12-10 17:14:27 +0300

    Update 2.php

--- site/2.php
+++ site/2.php
@@ -2,5 +2,5 @@
  include "probnik.php"; 
 $search = new iMarc\GoogleCustomSearch(012801828530494578547:7ddq3fkh7ew, AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA);
  $results = $search->search('Apples');
-Echo $result
+Echo $results;
  ?>
