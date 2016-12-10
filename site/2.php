bd9a06bc31c547295bb25db7f6fffca25aa0395a
Mashkin232 <0913757@mail.ru>
2016-12-10 20:32:52 +0300

    Delete 2.php

--- site/2.php
+++
@@ -1,6 +0,0 @@
-<?php
- include "probnik.php"; 
-$search = new iMarc\GoogleCustomSearch(012801828530494578547:7ddq3fkh7ew, AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA);
- $results = $search->search('Apples');
-Echo $results;
- ?>
