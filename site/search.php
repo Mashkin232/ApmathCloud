21e8b080cc46bc4a0ba9feb476f03a379a77bf6e
Mashkin232 <0913757@mail.ru>
2016-12-10 22:21:54 +0300

    Update search.php

--- site/search.php
+++ site/search.php
@@ -181,6 +181,7 @@ return $result;
 } 
 } 
 $search = new GoogleCustomSearch('012801828530494578547:7ddq3fkh7ew', 'AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA'); 
+	$value=$_POST('poisk')
 $results = $search->search("'.$value.'"); 
 foreach ($results->results as $result) { 
 echo $result['link']; 
