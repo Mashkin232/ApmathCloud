1556354567b0794b75d9b671db3a113758806769
Mashkin232 <0913757@mail.ru>
2016-12-10 21:56:22 +0300

    Update search.php

--- site/search.php
+++ site/search.php
@@ -173,7 +173,12 @@ return $result;
 } 
 } 
 $search = new GoogleCustomSearch('012801828530494578547:7ddq3fkh7ew', 'AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA'); 
-  
+ echo" <tr>
+			<td style="border:0px" colspan="2">Поиск:<input value="" name="poisk">
+			<button><font size=+1>Поиск</font></button>
+			</form>
+			</td>	
+		</tr>";
 $results = $search->search('Tallinn Hotels'); 
 foreach ($results->results as $result) { 
 echo $result['link']; 
