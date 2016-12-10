11e674686421b02ea6fefa7fc196819958281004
Mashkin232 <0913757@mail.ru>
2016-12-10 17:11:22 +0300

    Update 2.php

--- site/2.php
+++ site/2.php
@@ -1,2 +1,3 @@
- $search = new iMarc\GoogleCustomSearch(012801828530494578547:7ddq3fkh7ew, AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA);
+include "probnik.php"; 
+$search = new iMarc\GoogleCustomSearch(012801828530494578547:7ddq3fkh7ew, AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA);
  $results = $search->search('Apples');
