0db866df6e60a4565b9d6fa6689cf2c268e2fa18
Mashkin232 <0913757@mail.ru>
2016-12-10 20:11:11 +0300

    Update probnik.php

--- site/probnik.php
+++ site/probnik.php
@@ -173,7 +173,7 @@ return $result;
 } 
 } 
 $search = new GoogleCustomSearch('012801828530494578547:7ddq3fkh7ew', 'AIzaSyD7Vffkn2jWpPwwfSwHrVhMH4dBHGSl_AA'); 
-$results = $search->search('Apples'); 
+$results = $search->search('Comment'); 
 foreach ($results->results as $result) { 
 echo $result['link']; 
 } 
